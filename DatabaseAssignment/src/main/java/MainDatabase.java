e6758b2a0b42cb2c632727f8d48dd5e701718299
parththak <parth.thakkar@mail.utoronto.ca>
2019-07-18 19:04:22 -0400

    Added mysql connector to main database and added a ER model in mysql workbench

---
+++ DatabaseAssignment/src/main/java/MainDatabase.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author parththakkar
+ */
+public class MainDatabase {
+    
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "rootpassword";
+    private static final String CONN_STRING = 
+            "jdbc:mysql://localhost:3306/mybnb";
+    
+    
+    
+    public static void main(String[] args) {
+        
+        
+    }
+    
+}
