f6d047dde1297dcdd3909f44664d248e3c963fbd
leo <tianhao.yao@mail.utoronto.ca>
2019-07-22 16:49:49 -0400

    shut up parth

--- DatabaseAssignment/src/main/java/database/Ui.java
+++ DatabaseAssignment/src/main/java/database/Ui.java
@@ -1,4 +1,29 @@
 package database;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Ui {
+    private JButton buttonLogin;
+    private JPanel panelMain;
+    private JPasswordField passwordField;
+    private JTextField textUsername;
+
+    public Ui() {
+        buttonLogin.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                JOptionPane.showMessageDialog(null, "stfu");
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Ui");
+        frame.setContentPane(new Ui().panelMain);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
