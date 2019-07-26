4b310e956b08b7bc3edf10c8587e9bc0c1f59e41
Parth Thakkar <pthakkar100@gmail.com>
2019-07-25 21:05:57 -0400

    Search listing by name now shows entire table

--- DatabaseAssignment/src/main/java/database/SearchListing.java
+++ DatabaseAssignment/src/main/java/database/SearchListing.java
@@ -13,6 +13,7 @@ import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.DefaultListModel;
+import javax.swing.table.DefaultTableModel;
 
 /**
  *
@@ -23,8 +24,11 @@ public class SearchListing extends javax.swing.JFrame {
     /**
      * Creates new form SearchListing
      */
+    DefaultTableModel model ;
+
     public SearchListing() {
         initComponents();
+        model = (DefaultTableModel)listTable.getModel();
     }
 
     /**
@@ -41,8 +45,8 @@ public class SearchListing extends javax.swing.JFrame {
         jLabel2 = new javax.swing.JLabel();
         searchField = new javax.swing.JTextField();
         searchButton = new javax.swing.JButton();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        searchList = new javax.swing.JList<>();
+        tableDisplay = new javax.swing.JScrollPane();
+        listTable = new javax.swing.JTable();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -60,12 +64,24 @@ public class SearchListing extends javax.swing.JFrame {
             }
         });
 
-        searchList.setModel(new javax.swing.AbstractListModel<String>() {
-            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
-            public int getSize() { return strings.length; }
-            public String getElementAt(int i) { return strings[i]; }
-        });
-        jScrollPane1.setViewportView(searchList);
+        listTable.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null}
+            },
+            new String [] {
+                "List id", "Name", "Address", "Description", "Lat", "Long", "Amenities"
+            }
+        ));
+        tableDisplay.setViewportView(listTable);
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -76,16 +92,16 @@ public class SearchListing extends javax.swing.JFrame {
                 .addComponent(jLabel1)
                 .addGap(222, 222, 222))
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(175, 175, 175)
+                .addGap(201, 201, 201)
                 .addComponent(jLabel2)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(searchField, javax.swing.GroupLayout.DEFAULT_SIZE, 203, Short.MAX_VALUE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(searchField, javax.swing.GroupLayout.PREFERRED_SIZE, 132, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
                 .addComponent(searchButton)
-                .addGap(128, 128, 128))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jScrollPane1)
+                .addComponent(tableDisplay, javax.swing.GroupLayout.DEFAULT_SIZE, 588, Short.MAX_VALUE)
                 .addContainerGap())
         );
         jPanel1Layout.setVerticalGroup(
@@ -99,8 +115,8 @@ public class SearchListing extends javax.swing.JFrame {
                     .addComponent(jLabel2)
                     .addComponent(searchButton))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 347, Short.MAX_VALUE)
-                .addContainerGap())
+                .addComponent(tableDisplay, javax.swing.GroupLayout.PREFERRED_SIZE, 196, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(22, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -118,7 +134,7 @@ public class SearchListing extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void searchButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_searchButtonActionPerformed
-        // TODO add your handling code here:
+
         try {
             try {
                 Class.forName("com.mysql.jdbc.Driver");
@@ -138,10 +154,14 @@ public class SearchListing extends javax.swing.JFrame {
             ResultSet rs = ps.executeQuery();
             System.out.println("listing search works");
             DefaultListModel demoList = new DefaultListModel();
-
-            while (rs.next()) {
+            
+            int rowIndex = 0;
+            while (rs.next() && rowIndex < model.getRowCount()) {
                 System.out.println(rs.getString("list_address"));
-                
+                model.insertRow(rowIndex, 
+                        new Object [] {rs.getString("list_id"),rs.getString("list_name"),
+                            rs.getString("list_address"),rs.getString("description"),rs.getString("latitude"),rs.getString("longitude"),rs.getString("amenities")});
+                rowIndex++;
             }
           
         } catch (SQLException e){
@@ -176,6 +196,10 @@ public class SearchListing extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(SearchListing.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
+        
+        
+        
+        
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
@@ -189,9 +213,9 @@ public class SearchListing extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable listTable;
     private javax.swing.JButton searchButton;
     private javax.swing.JTextField searchField;
-    private javax.swing.JList<String> searchList;
+    private javax.swing.JScrollPane tableDisplay;
     // End of variables declaration//GEN-END:variables
 }
