954617c6753f432075a649f0a40e6f50a566b31c
Parth Thakkar <pthakkar100@gmail.com>
2019-07-28 17:29:20 -0400

    Fixed add listing column

--- DatabaseAssignment/src/main/java/database/AddListing.java
+++ DatabaseAssignment/src/main/java/database/AddListing.java
@@ -59,6 +59,8 @@ public class AddListing extends javax.swing.JFrame {
         countryField = new javax.swing.JTextField();
         jLabel10 = new javax.swing.JLabel();
         jLabel11 = new javax.swing.JLabel();
+        showPriceSuggestion = new javax.swing.JButton();
+        suggestedPrice = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -98,7 +100,7 @@ public class AddListing extends javax.swing.JFrame {
             }
         });
 
-        jLabel9.setText("Rent Amount");
+        jLabel9.setText("Rent Amount($)");
 
         postalCode.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -115,6 +117,16 @@ public class AddListing extends javax.swing.JFrame {
         jLabel10.setText("Postal code");
 
         jLabel11.setText("Country");
+
+        showPriceSuggestion.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
+        showPriceSuggestion.setText("Get suggested price $");
+        showPriceSuggestion.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                showPriceSuggestionActionPerformed(evt);
+            }
+        });
+
+        suggestedPrice.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -164,8 +176,12 @@ public class AddListing extends javax.swing.JFrame {
                                                 .addComponent(postalCode, javax.swing.GroupLayout.PREFERRED_SIZE, 58, javax.swing.GroupLayout.PREFERRED_SIZE)
                                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                                 .addComponent(countryField, javax.swing.GroupLayout.PREFERRED_SIZE, 77, javax.swing.GroupLayout.PREFERRED_SIZE)))))
-                                .addGap(0, 11, Short.MAX_VALUE)))
-                        .addGap(309, 309, 309))))
+                                .addGap(0, 0, Short.MAX_VALUE)))
+                        .addGap(30, 30, 30)
+                        .addComponent(showPriceSuggestion)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(suggestedPrice, javax.swing.GroupLayout.PREFERRED_SIZE, 56, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(144, 144, 144))))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jLabel1)
@@ -209,7 +225,10 @@ public class AddListing extends javax.swing.JFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jLabel9)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(rentAmount, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(rentAmount, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(showPriceSuggestion)
+                    .addComponent(suggestedPrice, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jLabel8)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
@@ -219,7 +238,7 @@ public class AddListing extends javax.swing.JFrame {
                     .addComponent(date, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addComponent(addListButton, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(36, Short.MAX_VALUE))
+                .addContainerGap(32, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -267,7 +286,7 @@ public class AddListing extends javax.swing.JFrame {
             
 
             
-            PreparedStatement ps2 = conn.prepareStatement("INSERT INTO `listing_dates` (listings_list_id,listing_dates) VALUES (LAST_INSERT_ID(),?)");
+            PreparedStatement ps2 = conn.prepareStatement("INSERT INTO `listing_dates` (listings_list_id,dates) VALUES (LAST_INSERT_ID(),?)");
             String finalDate = year.getText() + '-' + month.getText() + '-' + date.getText();
             ps2.setString(1,finalDate);
             ps2.executeUpdate();
@@ -293,6 +312,22 @@ public class AddListing extends javax.swing.JFrame {
     private void countryFieldActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_countryFieldActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_countryFieldActionPerformed
+
+    private void showPriceSuggestionActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_showPriceSuggestionActionPerformed
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }//GEN-LAST:event_showPriceSuggestionActionPerformed
 
     /**
      * @param args the command line arguments
@@ -354,6 +389,8 @@ public class AddListing extends javax.swing.JFrame {
     private javax.swing.JTextField month;
     private javax.swing.JTextField postalCode;
     private javax.swing.JTextField rentAmount;
+    private javax.swing.JButton showPriceSuggestion;
+    private javax.swing.JTextField suggestedPrice;
     private javax.swing.JTextField year;
     // End of variables declaration//GEN-END:variables
 }
